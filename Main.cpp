e67a46fc3c9d8deb25773f505165c8d091bfd898
GwansuKim <121069568+GwansuKim@users.noreply.github.com>
2025-09-22 11:04:27 +0900

    방식2 추가
    방식2 추가

--- Main.cpp
+++ Main.cpp
@@ -74,5 +74,23 @@ int main()
 		cout << "\n";
 	}
 
+	//*****
+	// ****
+	//  ***
+	//   **
+	//    *
+	for (int i = 1; i <= 5; i++)
+	{
+		for (int j = 1; j < i; j++)
+		{
+			cout << " ";
+		}
+		for (int j = i; j <= 5; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
 	return 0;
 }
