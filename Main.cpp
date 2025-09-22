2076fdc9e86de6d850383e13412237d74208113f
GwansuKim <121069568+GwansuKim@users.noreply.github.com>
2025-09-22 09:09:19 +0900

    add Mul of 3
    add Mul of 3

--- Main.cpp
+++ Main.cpp
@@ -5,12 +5,23 @@ using namespace std;
 int main()
 {
 	int sum = 0;
-	for (int i = 0; i < 101; i++)
+	for (int i = 1; i < 101; i++)
 	{
 		sum += i;
 	}
 
 	cout << "Summary : " << sum;
+
+	sum = 0;
+	for (int i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0)
+		{
+			sum += i;
+		}
+	}
+
+	cout << "Mul of 3 : " << sum;
 
 	return 0;
 }
